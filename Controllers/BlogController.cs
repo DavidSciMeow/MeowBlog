b79741cc35870b86623cdd8d5e56a7333b2c3904
Tennant David <DavidTennant405@outlook.com>
2022-03-23 14:24:48 +0800

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,88 @@
+﻿using MeowBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace MeowBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult Display(string id)
+		{
+			try
+			{
+                ViewData["blogitem"] = BlogManagement.DeserilizeBlogToModel(id);
+            }
+			catch
+			{
+                ViewData["blogitem"] = null;
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Insert()
+        {
+            var k = new StreamReader(Request.HttpContext.Request.Body, Encoding.Default).ReadToEnd();
+            var jo = JObject.Parse(k);
+            if (
+                Request.HttpContext.Session.IsRoot() || //session验证
+                (ProgramProperties.Password?.Equals(jo["pass"]?.ToString()) ?? false) //内部密码验证
+                )
+            {
+                if (jo != null)
+                {
+                    var name = jo["name"]?.ToString() ?? "";
+                    var desc = jo["desc"]?.ToString() ?? "";
+                    var visa = "yes".Equals(jo["isVisable"]?.ToString() ?? "yes");
+                    var text = jo["body"]?.ToString() ?? "";
+                    if (!string.IsNullOrEmpty(text))
+                    {
+                        BlogModel bm = new() {id=Guid.NewGuid().ToString().Replace("-",""),name = name,description=desc,isVisiable=visa,Text=text,publish=DateTime.Now };
+                        bm.Save();
+                        ViewData["sc"] = "1";
+                        return Content("0");
+                    }
+                    else
+                    {
+                        ViewData["sc"] = "0";
+                        return Json(new { result = 1 ,errs="Blog Empty, Not Emit to save"});
+                    }
+                }
+                else
+                {
+                    ViewData["sc"] = "0";
+                    return Json(new { result = -1, errs = "Request Body Empty" });
+                }
+            }
+            else
+            {
+                ViewData["sc"] = "0";
+                return Json(new { result = 2, errs = "No session indecated your blog id -> request process stops" });
+            }
+        }
+        public IActionResult Delete(string id)
+        {
+            if (Request.HttpContext.Session.IsRoot())//session验证
+            {
+                id.Delete();
+                return Redirect("/Blog/Index");
+            }
+            else
+            {
+                return Json(new { errs = "No session indecated you're administrator -> request process stops" });
+            }
+        }
+        public IActionResult AddBlog()
+        {
+            if (Request.HttpContext.Session.GetInt32("isRoot") == 0)
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("/Home/Login");
+            }
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using MeowBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Text;
+
+namespace MeowBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            try
+            {
+                ViewData["innerHtml"] = System.IO.File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", ProgramProperties.InnerIndexLayoutPage ?? "Basic/IndexLayout.html"));
+            }
+            catch
+            {
+                ViewData["innerHtml"] = "<div class=\"text-center\"><h1>Welcome Using Meow's Blog</h1><p>Please using IndexLayout.html to change this page</p><h2>欢迎使用喵的微型博客</h2><p>请设置Basic内的IndexLayout.html页更改本内容</p></div>";
+            }
+            return View();
+        }
+        public IActionResult Login() => View();
+        [HttpPost]
+        public IActionResult ApiLogin()
+        {
+            try
+            {
+                var jo = JObject.Parse(new StreamReader(Request.HttpContext.Request.Body, Encoding.Default).ReadToEnd());
+                if (ProgramProperties.Password?.Equals(jo["pass"]?.ToString()) ?? false)
+                {
+                    Request.HttpContext.Session.SetInt32("isRoot", 0);
+                    Request.HttpContext.Session.SetString("loginTime", $"{DateTime.Now:d}");
+                    return Content("0:null");
+                }
+                else
+                {
+                    return Content("1:passerr");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                $"On Method *Home-Login* Throw Err follows:{ex.Message}".ToLog(3);
+                return Content($"-1:{ex.Message}");
+            }
+        }
+        public IActionResult ApiLogout()
+        {
+            Request.HttpContext.Session.Clear();
+            return Redirect("/Home/Index");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
